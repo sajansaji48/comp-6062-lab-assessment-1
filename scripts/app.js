3d2382d1bd66129ee964e1fea3f2ec45ee22862e
sajansaji48 <s_saji209897@fanshaweonline.ca>
2025-02-13 20:02:20 -0500

    finalised

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ console.log();
 const studentFullName = 'Sajan Saji';
 const studentNumber = '1190959';
 const result= studentFullName + '-' + studentNumber;
-console.log(result);
-let headerContent = document.querySelector("message");
+console.log(result)
+let headerContent = document.querySelector(".message");
 headerContent.innerHTML=result;
 headerContent.classList.add("headingPrimary")
